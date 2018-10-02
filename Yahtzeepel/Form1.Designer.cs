475d1bf76e18680077728965502f234455ede17e
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-10-02 11:57:29 +0200

    Add files via upload

---
+++ Yahtzeepel/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Yahtzeepel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblD1 = new System.Windows.Forms.Label();
+            this.lblD2 = new System.Windows.Forms.Label();
+            this.lblD3 = new System.Windows.Forms.Label();
+            this.lblD4 = new System.Windows.Forms.Label();
+            this.lblD5 = new System.Windows.Forms.Label();
+            this.chbB1 = new System.Windows.Forms.CheckBox();
+            this.chbB2 = new System.Windows.Forms.CheckBox();
+            this.chbB3 = new System.Windows.Forms.CheckBox();
+            this.chbB4 = new System.Windows.Forms.CheckBox();
+            this.chbB5 = new System.Windows.Forms.CheckBox();
+            this.btnRol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblD1
+            // 
+            this.lblD1.AutoSize = true;
+            this.lblD1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD1.Location = new System.Drawing.Point(12, 9);
+            this.lblD1.Name = "lblD1";
+            this.lblD1.Size = new System.Drawing.Size(29, 31);
+            this.lblD1.TabIndex = 0;
+            this.lblD1.Text = "1";
+            // 
+            // lblD2
+            // 
+            this.lblD2.AutoSize = true;
+            this.lblD2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD2.Location = new System.Drawing.Point(12, 40);
+            this.lblD2.Name = "lblD2";
+            this.lblD2.Size = new System.Drawing.Size(29, 31);
+            this.lblD2.TabIndex = 1;
+            this.lblD2.Text = "1";
+            // 
+            // lblD3
+            // 
+            this.lblD3.AutoSize = true;
+            this.lblD3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD3.Location = new System.Drawing.Point(12, 74);
+            this.lblD3.Name = "lblD3";
+            this.lblD3.Size = new System.Drawing.Size(29, 31);
+            this.lblD3.TabIndex = 2;
+            this.lblD3.Text = "1";
+            // 
+            // lblD4
+            // 
+            this.lblD4.AutoSize = true;
+            this.lblD4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD4.Location = new System.Drawing.Point(12, 111);
+            this.lblD4.Name = "lblD4";
+            this.lblD4.Size = new System.Drawing.Size(29, 31);
+            this.lblD4.TabIndex = 3;
+            this.lblD4.Text = "1";
+            // 
+            // lblD5
+            // 
+            this.lblD5.AutoSize = true;
+            this.lblD5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD5.Location = new System.Drawing.Point(12, 148);
+            this.lblD5.Name = "lblD5";
+            this.lblD5.Size = new System.Drawing.Size(29, 31);
+            this.lblD5.TabIndex = 4;
+            this.lblD5.Text = "1";
+            // 
+            // chbB1
+            // 
+            this.chbB1.AutoSize = true;
+            this.chbB1.Location = new System.Drawing.Point(47, 23);
+            this.chbB1.Name = "chbB1";
+            this.chbB1.Size = new System.Drawing.Size(75, 17);
+            this.chbB1.TabIndex = 5;
+            this.chbB1.Text = "Behouden";
+            this.chbB1.UseVisualStyleBackColor = true;
+            // 
+            // chbB2
+            // 
+            this.chbB2.AutoSize = true;
+            this.chbB2.Location = new System.Drawing.Point(47, 54);
+            this.chbB2.Name = "chbB2";
+            this.chbB2.Size = new System.Drawing.Size(75, 17);
+            this.chbB2.TabIndex = 6;
+            this.chbB2.Text = "Behouden";
+            this.chbB2.UseVisualStyleBackColor = true;
+            // 
+            // chbB3
+            // 
+            this.chbB3.AutoSize = true;
+            this.chbB3.Location = new System.Drawing.Point(47, 88);
+            this.chbB3.Name = "chbB3";
+            this.chbB3.Size = new System.Drawing.Size(75, 17);
+            this.chbB3.TabIndex = 7;
+            this.chbB3.Text = "Behouden";
+            this.chbB3.UseVisualStyleBackColor = true;
+            // 
+            // chbB4
+            // 
+            this.chbB4.AutoSize = true;
+            this.chbB4.Location = new System.Drawing.Point(47, 125);
+            this.chbB4.Name = "chbB4";
+            this.chbB4.Size = new System.Drawing.Size(75, 17);
+            this.chbB4.TabIndex = 8;
+            this.chbB4.Text = "Behouden";
+            this.chbB4.UseVisualStyleBackColor = true;
+            // 
+            // chbB5
+            // 
+            this.chbB5.AutoSize = true;
+            this.chbB5.Location = new System.Drawing.Point(47, 162);
+            this.chbB5.Name = "chbB5";
+            this.chbB5.Size = new System.Drawing.Size(75, 17);
+            this.chbB5.TabIndex = 9;
+            this.chbB5.Text = "Behouden";
+            this.chbB5.UseVisualStyleBackColor = true;
+            // 
+            // btnRol
+            // 
+            this.btnRol.Location = new System.Drawing.Point(137, 12);
+            this.btnRol.Name = "btnRol";
+            this.btnRol.Size = new System.Drawing.Size(50, 50);
+            this.btnRol.TabIndex = 10;
+            this.btnRol.Text = "Rol";
+            this.btnRol.UseVisualStyleBackColor = true;
+            this.btnRol.Click += new System.EventHandler(this.btnRol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 270);
+            this.Controls.Add(this.btnRol);
+            this.Controls.Add(this.chbB5);
+            this.Controls.Add(this.chbB4);
+            this.Controls.Add(this.chbB3);
+            this.Controls.Add(this.chbB2);
+            this.Controls.Add(this.chbB1);
+            this.Controls.Add(this.lblD5);
+            this.Controls.Add(this.lblD4);
+            this.Controls.Add(this.lblD3);
+            this.Controls.Add(this.lblD2);
+            this.Controls.Add(this.lblD1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblD1;
+        private System.Windows.Forms.Label lblD2;
+        private System.Windows.Forms.Label lblD3;
+        private System.Windows.Forms.Label lblD4;
+        private System.Windows.Forms.Label lblD5;
+        private System.Windows.Forms.CheckBox chbB1;
+        private System.Windows.Forms.CheckBox chbB2;
+        private System.Windows.Forms.CheckBox chbB3;
+        private System.Windows.Forms.CheckBox chbB4;
+        private System.Windows.Forms.CheckBox chbB5;
+        private System.Windows.Forms.Button btnRol;
+    }
+}
+
---
+++ Yahtzeepel/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzeepel
+{
+    public partial class Form1 : Form
+    {
+        //
+        //Vars
+        //
+        Random rnd = new Random(); // hier maken we een random om te gebruiken voor de dobbelstenen
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DiceRol()
+        {
+            int result1 = rnd.Next(1, 7);
+            int result2 = rnd.Next(1, 7);
+            int result3 = rnd.Next(1, 7);
+            int result4 = rnd.Next(1, 7);
+            int result5 = rnd.Next(1, 7);
+
+            if (chbB1.Checked != true)
+            {
+                lblD1.Text = result1.ToString();
+            }
+            if (chbB2.Checked != true)
+            {
+                lblD2.Text = result2.ToString();
+            }
+            if (chbB3.Checked != true)
+            {
+                lblD3.Text = result3.ToString();
+            }
+            if (chbB4.Checked != true)
+            {
+                lblD4.Text = result4.ToString();
+            }
+            if (chbB5.Checked != true)
+            {
+                lblD5.Text = result5.ToString();
+            }
+        }
+
+        private void btnRol_Click(object sender, EventArgs e)
+        {
+            DiceRol();
+        }
+    }
+}
